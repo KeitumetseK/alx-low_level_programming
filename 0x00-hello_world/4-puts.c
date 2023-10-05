2ed05e15f566f5d9753af4f9d45f12a321d21adb
KeitumetseK <keitumetselovely@gmail.com>
2023-10-05 13:02:12 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
-
+	/**
+         * main - seves as a starting point for program exection
+         * return - ends the execution of a function
+         */
 int main (void)
 {
+	
 	puts("\"Programming is like building a multilingual puzzle);
 	Return (0);
 }
