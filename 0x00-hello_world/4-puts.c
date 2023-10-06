532ddf97fbe2f13017f4e7b3ef54a4999b7ecc50
KeitumetseK <keitumetselovely@gmail.com>
2023-10-06 01:57:29 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
 
 /**
-* main - serves as a starting point for program exection.
-* 
-* return - ends the execution of a function. Always returns 0.
+ * main - serves as a starting point for program exection. 
+ * return - ends the execution of a function. Always returns 0.
 */
 
 int main(void)
-{
-	
+{	
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
