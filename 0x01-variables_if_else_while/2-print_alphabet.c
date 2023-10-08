3cf84ab3cce45be7729c14e2b968b79dcd32fe75
KeitumetseK <keitumetselovely@gmail.com>
2023-10-08 01:52:08 -0700

    alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: ALways 0 (Success)
+ */
 
 int main(void)
 {
+	char i;
 
+	for (i = 'a'; i <= 'z' ; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
 }
