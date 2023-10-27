a6b8f602f13536720974637f4545b97705e5a0b8
KeitumetseK <keitumetselovely@gmail.com>
2023-10-27 14:50:39 -0700

    answer to

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+void _puts(char *str)
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *_puts - writes the character c to stdout*
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
+
---
+++ main.h
@@ -0,0 +1,22 @@
+#fndef MAIN_H
+		
+#define MAIN_H
+		
+
+		
+int _putchar(char c);
+		
+int _atoi(char *s);
+		
+
+		
+#endif
+
+ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+void positive_or_negative(int i);
+int largest_number(int a, int b, int c);
+void print_remaining_days(int month, int day, int year);
+int convert_day(int month, int day);
+#endif
